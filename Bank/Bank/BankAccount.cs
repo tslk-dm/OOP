1b49c279853f9a81a20ac8fa0dbbe743b5781a51
DmitriyTislyuk <tislyukdmitriy@mail.ru>
2022-03-27 15:31:49 +0300

    Урок 1. Задание 1, 2

---
+++ Bank/Bank/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class BankAccount
+    {
+        private int _accountNumber;
+        private int _balance;
+        private TypeBankAccount _typeBankAccount;
+
+        private static int numberOfAccount;
+
+        public BankAccount()
+        {
+            numberOfAccount += 1;
+            _accountNumber = numberOfAccount;
+        }
+
+        public int getAccountNumber()
+        {
+            return _accountNumber;
+        }
+
+        public int getBalance()
+        {
+            return _balance;
+        }
+
+        public TypeBankAccount getTypeBankAccount()
+        {
+            return _typeBankAccount;
+        }
+
+        public void setAccountNumber(int accountNumber)
+        {
+            _accountNumber = accountNumber;
+        }
+
+        public void setBalance(int balance)
+        {
+            _balance = balance;
+        }
+
+        public void setTypeBankAccount(TypeBankAccount typeBankAccount)
+        {
+            _typeBankAccount = typeBankAccount;
+        }
+
+    }
+}
--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bank
 {
@@ -6,7 +7,27 @@ namespace Bank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int balance = 100;
+            TypeBankAccount typeBankAccount = TypeBankAccount.CurrentAccount;
+
+            BankAccount bankAccount1 = new BankAccount();
+            BankAccount bankAccount2 = new BankAccount();
+            BankAccount bankAccount3 = new BankAccount();
+
+            List<BankAccount> bank = new List<BankAccount>();
+            bank.Add(bankAccount1);
+            bank.Add(bankAccount2);
+            bank.Add(bankAccount3);
+
+            foreach (var bankAccount in bank)
+            {
+                bankAccount.setBalance(balance);
+                bankAccount.setTypeBankAccount(typeBankAccount);
+                Console.WriteLine(bankAccount.getAccountNumber());
+                Console.WriteLine(bankAccount.getBalance());
+                Console.WriteLine(bankAccount.getTypeBankAccount());
+            }
+
         }
     }
 }
